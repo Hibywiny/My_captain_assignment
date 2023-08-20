3f90a4fc270b5ec7d0bd91c10c4f8887ea603f93
Hibywiny <141756789+Hibywiny@users.noreply.github.com>
2023-08-20 16:35:23 +0400

    Add files via upload

---
+++ area of circle.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import math
+print('What is the radius of your circle?')
+radius = input()
+
+radius = float(radius)
+area=radius**2*math.pi
+
+print("The area of the circle with the radius of", radius, "equals to",area )
+
+
