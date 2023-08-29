33215551090a0eff95240f0dd606da5f01bdd3e7
Hibywiny <141756789+Hibywiny@users.noreply.github.com>
2023-08-29 06:07:34 +0400

    Add files via upload

---
+++ extension - by Hibat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 20 16:36:59 2023
+
+@author: hibatallabenibella
+"""
+print('what is the filename?')
+filename=input()
+extension = filename.split('.')
+print('the extension is',extension[-1])
